25019e5e4a26350ff8d86c5e67a24a1de684ef81
RunnningDogg <15889666941@163.com>
2023-05-18 23:34:02 +0800

    change tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -202,22 +202,6 @@ module.exports = {
     boxShadow: ['focus', 'dark:focus'],
   },
   plugins: [
-    require('tailwindcss-multi-theme'),
-    require('@tailwindcss/forms'),
-    plugin(({ addUtilities, e, theme, variants }) => {
-      const newUtilities = {}
-      Object.entries(theme('colors')).map(([name, value]) => {
-        if (name === 'transparent' || name === 'current') return
-        const color = value[300] ? value[300] : value
-        const hsla = Color(color).alpha(0.45).hsl().string()
-
-        newUtilities[`.shadow-outline-${name}`] = {
-          'box-shadow': `0 0 0 3px ${hsla}`,
-        }
-      })
-
-      addUtilities(newUtilities, variants('boxShadow'))
-    }),
   ],
 }
 
