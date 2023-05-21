48d5ca0fbdc05370d8201fff71e8e2f53137d4f7
RunnningDogg <15889666941@163.com>
2023-05-22 03:21:27 +0800

    login vue component

--- src/pages/common/Login.vue
+++ src/pages/common/Login.vue
@@ -1,14 +1,14 @@
 
 <template>
-  <div class="login flex w-full h-full">
+  <div class="login flex  ">
 
     <!--    <img src="./login2.jpg" class="flex-1 object-cover md:h-auto md:w-1/2 p-6   ">-->
 
-    <div class="flex flex-col  w-full h-full items-center justify-center  ">
+    <div class="flex flex-col   items-center justify-center  ">
 
-      <form class="w-8/12 h-full flex flex-col justify-center items-center  ">
+      <form class="flex flex-col justify-center items-center  ">
 
-        <h1 class="mb-4 text-xl font-semibold
+        <h1 class="mb-4 text-xl font-semibold 
         text-gray-700 dark:text-gray-200 text-center">
           登录
         </h1>
@@ -28,8 +28,8 @@
         <input class="block w-6/12 mt-1 text-sm rounded
           border-gray-200 dark:border-gray-600 dark:bg-gray-700
           focus:border-red-300 focus:outline-none focus:shadow-outline-red
-          dark:text-gray-300 dark:focus:shadow-outline-gray form-input mb-8" placeholder="***" type="password"
-          id="password" name="password" required v-model="password" />
+           form-input mb-8" placeholder="***" type="password" id="password" name="password" required
+          v-model="password" />
 
         <button type="submit" class="inline-block w-6/12 h-10 rounded-md
             bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500
@@ -38,15 +38,12 @@
         </button>
       </form>
     </div>
-
-
-
   </div>
 </template>
 
 <script setup>
 
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
 import { ElMessage, ElNotification } from 'element-plus'
